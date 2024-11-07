0316a08c33af4cf24906c3b74d5432f7e2f6fcf9
Manav Kumar Sharma <100007272.joan23@fje.edu>
2024-11-07 15:35:39 +0100

    Primera revisión de códgio

--- php/procLogin.php
+++ php/procLogin.php
@@ -9,7 +9,7 @@ session_start();
 if (isset($_POST['user']) && isset($_POST['contrasena'])) {
     // Obtenemos los valores de los campos de formulario
     $username = mysqli_real_escape_string($conn, $_POST['user']);
-    $password = $_POST['contrasena'];
+    $password = mysqli_real_escape_string($conn, $_POST['contrasena']);
 
     // Consulta para obtener el usuario y la contraseña en texto plano
     $query = "SELECT id_camarero, username_password FROM tbl_camarero WHERE username = '$username'";
@@ -21,10 +21,10 @@ if (isset($_POST['user']) && isset($_POST['contrasena'])) {
         $stored_password = $row['username_password'];
 
         // Verificamos si las contraseñas coinciden (sin encriptación)
-        if ($password === $stored_password) {
+        if (password_verify($password, $stored_password)) {
             // La autenticación es exitosa
             $_SESSION['user_id'] = $row['id_camarero']; // Guardamos el ID del usuario en la sesión
-            header("Location: ../mesas.php"); // Redirigimos a mesas.php
+            header("Location: ../view/mesas.php"); // Redirigimos a mesas.php
             exit();
         } else {
             // Contraseña incorrecta
--- view/mesas.php
+++ view/mesas.php
@@ -1,3 +1,21 @@
+<?php
+// Incluimos el archivo de conexión a la base de datos
+include '../php/conexion.php';
+
+// Iniciamos la sesión
+session_start();
+
+// Verificamos si la sesión del camarero está activa
+if (!isset($_SESSION['user_id'])) {
+    // Si no está activo, redirigimos a la página de inicio de sesión
+    header("Location: ./index.php");
+    exit();
+}
+
+// Obtenemos el ID del camarero desde la sesión
+$id_camarero = $_SESSION['user_id'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +23,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mesas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/mesas.css">
 </head>
 <body>
     <!-- Cabecera -->
